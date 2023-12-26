7accc417c45801182eee2c40c2ae0ad62cec3507
nikkbh <nikhilbhutani111@yahoo.com>
2023-12-26 14:05:51 +0530

    feat: add invite friends to server

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -3,9 +3,8 @@
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 
-import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
-import { channel } from "diagnostics_channel";
+import { cn } from "@/lib/utils";
 
 interface NavigationItemsProps {
   id: string;
@@ -27,7 +26,7 @@ export const NavigationItem = ({
 
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <button onClick={() => {}} className="group relative flex items-center">
+      <button onClick={onClick} className="group relative flex items-center">
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
@@ -37,7 +36,7 @@ export const NavigationItem = ({
         />
         <div
           className={cn(
-            "relative flex mx-3 h-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
             params?.serverId === id &&
               "bg-primary/10 text-primary rounded-[16px]"
           )}
